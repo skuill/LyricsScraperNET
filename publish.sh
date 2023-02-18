d37235912aac6283ca233f564b95e61abc72c856
Vitaliy <sativkv@gmail.com>
2023-02-19 00:36:06 +0400

    Create publish.sh

---
+++ publish.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+echo "Publishing..."
+
+cd ./src/LyricsScraperNET
+dotnet pack -c Release LyricsScraperNET.csproj -p:PackageVersion="$RELEASE_VERSION"
+nuget push "./bin/Release/LyricsScraperNET.$RELEASE_VERSION.nupkg"\
+  -ApiKey "$NUGET_TOKEN"\
+  -NonInteractive\
+  -Source https://www.nuget.org/api/v2/package
+
+cd ..
