1607ed0fbf0590b561b28cf82b0df3e14f2d77b4
skuill <sativkv@gmail.com>
2023-08-21 00:50:32 +0400

    Fixed paths in tests. Try 2

--- LyricsScraperNET.TestShared/Utils/Serializer.cs
+++ LyricsScraperNET.TestShared/Utils/Serializer.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
 
 namespace LyricsScraperNET.TestShared.Utils
@@ -8,7 +11,8 @@ namespace LyricsScraperNET.TestShared.Utils
     {
         public static T Deseialize<T>(string path)
         {
-            var jsonString = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, path));
+            IEnumerable<string> paths = new List<string> { Environment.CurrentDirectory }.Concat(path.Split(new char[] { '\\', '/' }));
+            var jsonString = File.ReadAllText(Path.Combine(paths.ToArray()));
             return JsonSerializer.Deserialize<T>(jsonString);
         }
     }
