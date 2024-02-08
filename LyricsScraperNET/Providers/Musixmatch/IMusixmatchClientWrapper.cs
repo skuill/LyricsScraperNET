2a69071e23c21699c7822511a28b58096a804a54
skuill <sativkv@gmail.com>
2024-02-08 17:47:21 +0400

    #21. Fixed Musixmatch token regeneration logic. MusixmatchProvider refactoring.

---
+++ LyricsScraperNET/Providers/Musixmatch/IMusixmatchClientWrapper.cs
@@ -0,0 +1,15 @@
+﻿using LyricsScraperNET.Models.Responses;
+using System.Threading.Tasks;
+
+namespace LyricsScraperNET.Providers.Musixmatch
+{
+    /// <summary>
+    /// Decorator for the <seealso cref="MusixmatchClientLib.MusixmatchClient"/>
+    /// </summary>
+    public interface IMusixmatchClientWrapper
+    {
+        SearchResult SearchLyric(string artist, string song, bool regenerateToken = false);
+
+        Task<SearchResult> SearchLyricAsync(string artist, string song, bool regenerateToken = false);
+    }
+}
---
+++ LyricsScraperNET/Providers/Musixmatch/IMusixmatchTokenCache.cs
@@ -0,0 +1,15 @@
+﻿namespace LyricsScraperNET.Providers.Musixmatch
+{
+    /// <summary>
+    /// Token cache provider
+    /// </summary>
+    public interface IMusixmatchTokenCache
+    {
+        /// <summary>
+        /// Get or create token from cache. 
+        /// </summary>
+        /// <param name="regenerate">If true, then the token will be created again.</param>
+        /// <returns></returns>
+        string GetOrCreateToken(bool regenerate = false);
+    }
+}
