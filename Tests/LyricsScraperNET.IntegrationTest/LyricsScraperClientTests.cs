4e8327c9f1ec50a49b1c966a199bcb00a69819cd
Виталий Квашин <sativkv@gmail.com>
2025-02-03 22:50:45 +0400

    Added integration tests if lyrics not found

--- Tests/LyricsScraperNET.IntegrationTest/LyricsScraperClientTests.cs
+++ Tests/LyricsScraperNET.IntegrationTest/LyricsScraperClientTests.cs
@@ -54,5 +54,51 @@ namespace LyricsScraperNET.IntegrationTest
             Assert.NotNull(searchResult.LyricText);
             Assert.False(searchResult.Instrumental);
         }
+
+        [Fact]
+        public async Task SearchLyric_WithNotExistedLyric_ShouldReturnNotFound()
+        {
+            // Arrange
+            string artistToSearch = "SomeNotExistedArtist12341235";
+            string songToSearch = "SomeNotExistedLyric12341235";
+            ILyricsScraperClient lyricsScraperClient
+                = new LyricsScraperClient()
+                    .WithAllProviders();
+
+            var searchRequest = new ArtistAndSongSearchRequest(artistToSearch, songToSearch);
+
+            // Act
+            var searchResult = lyricsScraperClient.SearchLyric(searchRequest);
+
+            // Assert
+            Assert.NotNull(searchResult);
+            Assert.True(searchResult.IsEmpty());
+            Assert.Equal(ResponseStatusCode.NoDataFound, searchResult.ResponseStatusCode);
+            Assert.True(string.IsNullOrEmpty(searchResult.LyricText));
+            Assert.False(searchResult.Instrumental);
+        }
+
+        [Fact]
+        public async Task SearchLyricAsync_WithNotExistedLyric_ShouldReturnNotFound()
+        {
+            // Arrange
+            string artistToSearch = "SomeNotExistedArtist12341235";
+            string songToSearch = "SomeNotExistedLyric12341235";
+            ILyricsScraperClient lyricsScraperClient
+                = new LyricsScraperClient()
+                    .WithAllProviders();
+
+            var searchRequest = new ArtistAndSongSearchRequest(artistToSearch, songToSearch);
+
+            // Act
+            var searchResult = await lyricsScraperClient.SearchLyricAsync(searchRequest);
+
+            // Assert
+            Assert.NotNull(searchResult);
+            Assert.True(searchResult.IsEmpty());
+            Assert.Equal(ResponseStatusCode.NoDataFound, searchResult.ResponseStatusCode);
+            Assert.True(string.IsNullOrEmpty(searchResult.LyricText));
+            Assert.False(searchResult.Instrumental);
+        }
     }
 }
